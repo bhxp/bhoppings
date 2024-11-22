dbb7b45980e54b60c63d67808c30bedbc20824f0
ChezLinkZ <143735617+ChezLinkZ@users.noreply.github.com>
2024-11-21 19:44:45 -0800

    add 404 message

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send('404: Page Not Found');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
