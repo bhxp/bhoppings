0f11f5e7b9a418b49a14a7edd3538df3484d2530
ChezLinkZ <143735617+ChezLinkZ@users.noreply.github.com>
2025-01-30 10:43:11 -0800

    replit settings push

--- public/settings/settings.js
+++ public/settings/settings.js
@@ -1,15 +1,28 @@
 function setCursor(number) {
-    window.localStorage.setItem("cursor", JSON.stringify(number));
-    cursor.setAttribute(
-      "src",
-      `/images/cursor/${window.localStorage.cursor}.svg`,
-    );
-  }
-  
-  function cursorSettings() {
-    $("#settings-container button").click(function () {
-      setCursor($("#settings-container button").index(this));
-    });
-  }
-  
-  cursorSettings();  
+  window.localStorage.setItem("cursor", JSON.stringify(number));
+  cursor.setAttribute(
+    "src",
+    `/images/cursor/${window.localStorage.cursor}.svg`,
+  );
+  updateCursorSettings();
+}
+
+function updateCursorSettings() {
+  $("#cursor-sidebar-button img").attr(
+    "src",
+    `/images/cursor/${window.localStorage.cursor}.svg`,
+  );
+  $("#settings-container .option").removeClass("active-cursor");
+  $("#settings-container .option")
+    .eq(JSON.parse(window.localStorage.cursor))
+    .addClass("active-cursor");
+}
+
+function cursorSettings() {
+  $("#settings-container button").click(function () {
+    setCursor($("#settings-container button").index(this));
+  });
+  updateCursorSettings();
+}
+
+cursorSettings();
